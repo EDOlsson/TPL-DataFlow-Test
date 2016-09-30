b7c67edd5110843e757f452dacf5235bf5a417fe
Eric Olsson <edolsson@live.com>
2016-09-30 14:39:25 -0500

    Start developing Back-end service

--- DataFlow.Console/BackEnd/BackEndService.cs
+++ DataFlow.Console/BackEnd/BackEndService.cs
@@ -15,7 +15,7 @@ namespace BackEnd
         readonly DataStore _dataStore;
         readonly IFakeSpectraService _spectraService;
 
-        readonly TransformBlock<BackEndRequest, ServiceRequest> _dataStoreBlock;
+        readonly TransformBlock<DataStoreRequest, ServiceRequest> _dataStoreBlock;
         readonly TransformBlock<ServiceRequest, object> _spectraServiceBlock;
 
         public BackEndService() : this(new DataStore(), new FakeSpectraService()) { }
@@ -24,16 +24,65 @@ namespace BackEnd
         {
             _dataStore = dataStore;
             _spectraService = spectraService;
+
+            _dataStoreBlock = new TransformBlock<DataStoreRequest, ServiceRequest>(r => ProcessRequest(r));
+            _spectraServiceBlock = new TransformBlock<ServiceRequest, object>(r => ContactSpectraService(r));
+
+            _dataStoreBlock.LinkTo(_spectraServiceBlock);
+        }
+
+        ServiceRequest ProcessRequest(DataStoreRequest request)
+        {
+            var initRequest = request as CreateSessionRequest;
+            if (initRequest != null)
+            {
+                var id = _dataStore.Initialize(initRequest.Age);
+
+                return new NoOpServiceRequestWithId(id);
+            }
+
+            var postRequest = request as PostDataToSessionRequest;
+            if(postRequest != null)
+            {
+                return new PostFetalDataServiceRequest(postRequest.Id, postRequest.FetalData);
+            }
+
+            return ServiceRequest.EmptyRequest;
+        }
+
+        object ContactSpectraService(ServiceRequest request)
+        {
+            System.Diagnostics.Trace.WriteLine($"Processing request {request}.");
+
+            var initializeRequest = request as NoOpServiceRequestWithId;
+            if (initializeRequest != null)
+                return initializeRequest.Id;
+
+            var postRequest = request as PostFetalDataServiceRequest;
+            if(postRequest != null)
+            {
+                _spectraService.PostFetalData(postRequest.Id, postRequest.FetalData);
+                return null;
+            }
+
+            return null;
         }
 
         public async Task<SessionIdentifier> InitializeAsync(int age)
         {
-            return await _dataStore.InitializeAsync(age);
+            _dataStoreBlock.Post(new CreateSessionRequest(age));
+
+            var sessionId = await _spectraServiceBlock.ReceiveAsync();
+
+            return (SessionIdentifier)sessionId;
         }
 
         public async Task PostDataAsync(SessionIdentifier id, Data data)
         {
-            return;
+            _dataStoreBlock.Post(new PostDataToSessionRequest(id, data));
+
+            var t = _spectraServiceBlock.ReceiveAsync();
+            await t.ConfigureAwait(false);
         }
 
         public async Task<object> FetchAnalyticsAsync(SessionIdentifier id)
@@ -50,6 +99,93 @@ namespace BackEnd
             DeleteAll
         }
 
-        abstract class ServiceRequest { }
+        class ServiceRequest
+        {
+            public static readonly ServiceRequest EmptyRequest = new ServiceRequest();
+
+            protected ServiceRequest() { }
+
+            public override string ToString() => "Empty request";
+        }
+
+        class NoOpServiceRequestWithId : ServiceRequest
+        {
+            public SessionIdentifier Id { get; }
+
+            public NoOpServiceRequestWithId(SessionIdentifier id)
+            {
+                Id = id;
+            }
+
+            public override string ToString() => string.Format($"No-op service request with Id {Id}.");
+        }
+
+        class PostFetalDataServiceRequest : ServiceRequest
+        {
+            public SessionIdentifier Id { get; }
+
+            public Data FetalData { get; }
+
+            public PostFetalDataServiceRequest(SessionIdentifier id, Data fetalData)
+            {
+                Id = id;
+                FetalData = fetalData;
+            }
+
+            public override string ToString() => string.Format($"Post fetal data for session {Id} : {FetalData}.");
+        }
+
+
+        abstract class DataStoreRequest { }
+
+        class CreateSessionRequest : DataStoreRequest
+        {
+            public int Age { get; }
+
+            public CreateSessionRequest(int age)
+            {
+                Age = age;
+            }
+        }
+
+        abstract class IdSpecificDataStoreRequest : DataStoreRequest
+        {
+            public SessionIdentifier Id { get; }
+
+            protected IdSpecificDataStoreRequest(SessionIdentifier id)
+            {
+                Id = id;
+            }
+        }
+
+        class FetchSessionRequest : IdSpecificDataStoreRequest
+        {
+            public FetchSessionRequest(SessionIdentifier id) : base(id)
+            {
+            }
+        }
+
+        class DeleteSessionRequest : IdSpecificDataStoreRequest
+        {
+            public DeleteSessionRequest(SessionIdentifier id) : base(id)
+            {
+            }
+        }
+
+        class PostDataToSessionRequest : IdSpecificDataStoreRequest
+        {
+            public Data FetalData { get; }
+
+            public PostDataToSessionRequest(SessionIdentifier id, Data fetalData) : base(id)
+            {
+                FetalData = fetalData;
+            }
+        }
+
+        class DeleteAllSessionsRequest : DataStoreRequest
+        {
+
+        }
+
     }
 }
--- DataFlow.Console/BackEnd/DataStore.cs
+++ DataFlow.Console/BackEnd/DataStore.cs
@@ -11,142 +11,28 @@ namespace BackEnd
 {
     class DataStore
     {
-        readonly SerializedDictionary<string> _dataStore = new SerializedDictionary<string>();
-
         public DataStore()
         {
-
-        } 
-
-        public async Task<SessionIdentifier> InitializeAsync(int age)
-        {
-            return await _dataStore.AddNew(age);
+            TheData = new Dictionary<SessionIdentifier, int>();
         }
 
-        class SerializedDictionary<T>
+        public SessionIdentifier Initialize(int age)
         {
-            readonly Dictionary<SessionIdentifier, T> _dataStore = new Dictionary<SessionIdentifier, T>();
-            readonly TransformBlock<DataStoreRequest, SessionIdentifier> _queue;
-            readonly TransformBlock<SessionIdentifier, T> _dictionaryBlock;
+            var id = new SessionIdentifier(CalculateNextId());
 
-            public SerializedDictionary()
-            {
-                _queue = new TransformBlock<DataStoreRequest, T>(r => ProcessRequest(r));
-            }
+            TheData[id] = TheData.Values.Any() ? TheData.Values.Max() + 10 : 10;
 
-            public async Task<T> AddNew(int age)
-            {
-                _queue.Post(new CreateSessionRequest(age));
-
-                return await _queue.ReceiveAsync();
-            }
-
-            public async Task<T> Fetch(SessionIdentifier id)
-            {
-                _queue.Post(new FetchSessionRequest(id));
-
-                return await _queue.ReceiveAsync();
-            }
-
-            public Task Delete(SessionIdentifier id)
-            {
-                _queue.Post(new DeleteSessionRequest(id));
-
-                var t = _queue.ReceiveAsync();
-                t.ConfigureAwait(false);
-
-                return t;
-            }
-
-            public Task DeleteAll()
-            {
-                _queue.Post(new DeleteAllSessionsRequest());
-
-                var t = _queue.ReceiveAsync();
-                t.ConfigureAwait(false);
-
-                return t;
-            }
-
-            T ProcessRequest(DataStoreRequest request)
-            {
-                var createRequest = request as CreateSessionRequest;
-                if (createRequest != null)
-                {
-                    return CreateNewSession(createRequest);
-                }
-
-                var fetchRequest = request as FetchSessionRequest;
-                if (fetchRequest != null)
-                    return _dataStore[fetchRequest.Id];
-
-                var deleteRequest = request as DeleteSessionRequest;
-                if(deleteRequest != null)
-                {
-                    _dataStore.Remove(deleteRequest.Id);
-                    return default(T);
-                }
-
-                var deleteAllRequest = request as DeleteAllSessionsRequest;
-                if(deleteAllRequest != null)
-                {
-                    _dataStore.Clear();
-                    return default(T);
-                }
-
-                throw new InvalidOperationException($"Unable to process {request}.");
-            }
-
-            T CreateNewSession(CreateSessionRequest r)
-            {
-                var id = new SessionIdentifier(_dataStore.Keys.Select(k => k.Id).Max() + 1);
-                var value = default(T);
-
-                _dataStore.Add(id, value);
-
-                return value;
-            }
+            return id;
         }
-    }
 
-    abstract class DataStoreRequest { }
-
-    class CreateSessionRequest : DataStoreRequest
-    {
-        public int Age { get; }
-
-        public CreateSessionRequest(int age)
+        int CalculateNextId()
         {
-            Age = age;
+            if (!TheData.Keys.Any())
+                return 0;
+
+            return TheData.Keys.Max(k => k.Id) + 1;
         }
-    }
 
-    abstract class IdSpecificDataStoreRequest : DataStoreRequest
-    {
-        public SessionIdentifier Id { get; }
-
-        protected IdSpecificDataStoreRequest(SessionIdentifier id)
-        {
-            Id = id;
-        }
-    }
-
-    class FetchSessionRequest : IdSpecificDataStoreRequest
-    {
-        public FetchSessionRequest(SessionIdentifier id) : base(id)
-        {
-        }
-    }
-
-    class DeleteSessionRequest : IdSpecificDataStoreRequest
-    {
-        public DeleteSessionRequest(SessionIdentifier id) : base(id)
-        {
-        }
-    }
-
-    class DeleteAllSessionsRequest : DataStoreRequest
-    {
-
+        internal IDictionary<SessionIdentifier, int> TheData { get; }
     }
 }
